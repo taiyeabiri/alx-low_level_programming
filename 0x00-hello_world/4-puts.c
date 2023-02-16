ce10a2387c14493899ef5cb1ce78c09031286d3b
taiyeabiri <taiye.abiri@gmail.com>
2023-02-16 12:58:42 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program that prints with a puts function followed by a new line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
