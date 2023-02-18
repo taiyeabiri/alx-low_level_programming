0a8036cefb85edbda8a3683235a7709ce8de3d6c
taiyeabiri <taiye.abiri@gmail.com>
2023-02-17 16:01:39 -0800

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - a program that prints in lowercase followed by a new line
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char i;
+for (i = 'a' ; i <= 'z' ; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
