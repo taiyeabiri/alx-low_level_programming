e0648aba27e80a5af1c0cbceb94d13621ca016f1
taiyeabiri <taiye.abiri@gmail.com>
2023-02-21 17:35:46 -0800

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,10 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_sign -function that prints signs
+ * print_sign - function that prints signs
  *
- * @n: function paramenter
+ * @n: function parameter
  *
  * Return: 1, 0 and -1
  */
@@ -13,22 +14,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
 		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
 		return (0);
 	}
 	else
 	{
 		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
 		return (-1);
 	}
 }
