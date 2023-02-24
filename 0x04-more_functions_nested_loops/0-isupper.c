f4b8f834d1b874565d6e235711b3fc9e8cd4c50c
taiyeabiri <taiye.abiri@gmail.com>
2023-02-23 18:03:29 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* _isupper - checks for uppercase character
+* @c: Variable text
+*
+* Return: 0 (Success)
+*/
+
+int _isupper(int c)
+{
+
+	if (c >= 'A' && c <= 'Z')
+
+	{
+
+	return (1);
+
+	}
+
+	else
+
+	{
+
+	return (0);
+
+	}
+
+}
