c0dae0f5e87ebe3df78aef430e2360b3b8ebda5f
taiyeabiri <taiye.abiri@gmail.com>
2023-03-10 15:13:07 -0800

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,33 +1,36 @@
 #include "main.h"
 
- /**
+/**
  * _strcat - concatenates two strings
- * @dest: input value
- * @src: input value
+ * @dest: the destination string where the resulting string will be stored
+ * @src: the source string that will be concatenated to the end of dest
  *
- * Return: void	
+ * Return: a pointer to the destination string
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int i, j;
 
-
+	/* Find the length of the destination string */
 	i = 0;
 	while (dest[i] != '\0')
 	{
-		i++;
+	i++;
 	}
+
+	/* Append the source string to the end of the destination string */
 	j = 0;
 	while (src[j] != '\0')
 	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}		
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
 
-
+	/* Add a null terminator to the end of the resulting string */
 	dest[i] = '\0';
+
+	/* Return a pointer to the destination string */
 	return (dest);
 }
