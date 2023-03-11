7514ff14ac642b94e46639074e750f1faf4673a1
taiyeabiri <taiye.abiri@gmail.com>
2023-03-10 20:28:06 -0800

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,75 +3,58 @@
 #include <ctype.h>
 #include <string.h>
 
-	/**
-	 * check_num - check if a string contains only digit
-	 * @str: the string to check
-	 *
-	 * Return: 1 if the string contains only digits, 0 otherwise
-	 */
+/**
+ * check_num - checks if a string contains only digits
+ * @str: the string to check
+ *
+ * Return: 1 if the string contains only digits, 0 otherwise
+ */
 
 int check_num(char *str)
+{
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str))
 	{
-		unsigned int count;
-
-
-		count = 0;
-		while (count < strlen(str))
-
-
+		if (!isdigit(str[count]))
 		{
-			if (!isdigit(str[count]))
-			{
-				return (0);
-			}
-
-
-			count++;
+			return (0);
 		}
-		return (1);
+		count++;
+	}
+	return (1);
+}
+
+/**
+ * main - entry point for the program
+ * @argc: the number of arguments passed to the program
+ * @argv: an array of strings containing the arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+	while (count < argc)
+	{
+		if (check_num(argv[count]))
+		{
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		count++;
 	}
 
-	/**
-	 * main - entry point for the program
-	 * @argc: the number of arguments passed to the program
-	 * @argv: an array of strings containing the arguments
-	 *
-	 * Return: 0 on success, 1 on error
-	 */
-
-	int main(int argc, char *argv[])
-
-
-	{
-
-		int count;
-		int str_to_int;
-		int sum = 0;
-
-
-		count = 1;
-		while (count < argc)
-		{
-			if (check_num(argv[count]))
-
-			{
-				str_to_int = atoi(argv[count]);
-				sum += str_to_int;
-			}
-
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-
-
-			count++;
-		}
-
-
-		printf("%d\n", sum);
-
-
-		return (0);
-	}
+	printf("%d\n", sum);
+	return (0);
+}
